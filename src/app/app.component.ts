a53c56a9190937757987bae34da5931aa4c5fdaf
Julian <Henao>
2022-03-31 10:16:29 -0500

    Crea componente modal que se despliega desde la acción de un botón en el app-component para mostrar data de ejemplo injectada con MAT_DIALOG_DATA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from './components/shared/modal/modal.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-material-modal-project';
+  example = 'Modal';
+  img = '../assets/images/logodmscolor.svg';
+
+  constructor(public dialog: MatDialog){}
+
+  openDialog(){
+   let dialogRef = this.dialog.open(ModalComponent, {
+     data:{
+       titulo: 'Modal DMS',
+       contenido: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vehicula enim id elit dictum suscipit. Sed sed tortor eu nulla aliquet pellentesque. Sed sed tincidunt lectus, quis interdum lacus. Aliquam a justo felis. Nulla odio felis, consequat in tempus vel, malesuada a tortor. Donec nulla eros, fermentum eget magna in, condimentum condimentum quam. Nullam hendrerit neque et tortor aliquam, id interdum risus convallis. Curabitur quis mattis velit. Cras blandit justo vitae libero laoreet dignissim. Morbi hendrerit nec quam vel ultricies.'
+      }
+   });
+
+   dialogRef.afterClosed().subscribe(result => {
+     console.log(`Acción Modal: ${result}`);
+   });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { ModalComponent } from './components/shared/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule
   ],
+  entryComponents: [ModalComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
